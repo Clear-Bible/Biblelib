1082391b2ad886d72c8a41043a414d0d6139923b
Sean Boisen <sean@seanboisen.com>
2022-11-01 17:15:49 -0700

    Delete test_mappings.py
    Moved to a higher directory

--- biblelib/tests/test_mappings.py
+++
@@ -1,8 +0,0 @@
-"""Test code in maculagreek/mappings.py."""
-
-import pytest
-from ../maculagreek import mappings
-
-class TestMappings:
-    """Test basic functionality of Mappings class."""
-    pass
