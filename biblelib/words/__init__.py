b9c34d4c93c492f4c44be861322baa6bd6fa2af6
Sean Boisen <sean@seanboisen.com>
2022-11-02 22:43:46 -0700

    Added comments, imports

--- biblelib/words/__init__.py
+++ biblelib/words/__init__.py
@@ -1,1 +1,19 @@
-"""Utilities for working with word-level data from the Hebrew Bible and Greek New Testament."""
+"""This package provides utilities for working with word-level data from the Hebrew Bible and Greek New Testament.
+
+The Hebrew and Greek source files are available from
+
+*
+  [Clear-Bible/macula-hebrew](https://github.com/Clear-Bible/macula-hebrew):
+  Syntax trees, morphology, and linguistic annotations for the Hebrew
+  Bible
+
+*
+  [Clear-Bible/macula-greek](https://github.com/Clear-Bible/macula-greek):
+  Syntax trees, morphology, and linguistic annotations for the Greek
+  New Testament
+
+"""
+
+from .mappings import ClearID, Mapping, Mappings
+
+__all__ = ["ClearID", "Mapping", "Mappings"]
