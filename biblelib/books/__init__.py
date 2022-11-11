49811e18d96bcdee75860d361b9727c0fcc4b647
Sean Boisen <sean@seanboisen.com>
2022-11-10 17:55:57 -0800

    Improved docs

--- biblelib/books/__init__.py
+++ biblelib/books/__init__.py
@@ -1,4 +1,8 @@
-"""This package provides utilities for working with Bible book metadata and collections."""
+"""This package provides utilities for working with Bible book metadata and collections.
+
+`books` includes mappings between conventional reference systems, and canon lists.
+
+"""
 
 from .books import Book, Books, ProtestantCanon, CatholicCanon
 
