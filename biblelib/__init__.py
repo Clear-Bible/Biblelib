934798ccc3255de3ded7177696f5646ff037bcf5
Sean Boisen <sean@seanboisen.com>
2022-11-10 17:54:52 -0800

    Simplified
    Don't export names from sub-packages: too much potential for confusion.

--- biblelib/__init__.py
+++ biblelib/__init__.py
@@ -1,17 +1,6 @@
 """Biblelib provides utilities for working with Bible books, references, pericopes, and other units.
 
-More documentation here.
+This namespace is intentionally bare: see individual packages
+(`books`, `words`, etc.) for imports.
+
 """
-
-from .books import Book, Books, CatholicCanon, ProtestantCanon
-from .words import ClearID, Mapping, Mappings
-
-__all__ = [
-    # books
-    "Book",
-    "Books",
-    # words
-    "ClearID",
-    "Mapping",
-    "Mappings",
-]
