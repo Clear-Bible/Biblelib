97c2be004ac177950a70337693b025aa593894b0
Sean Boisen <sean@seanboisen.com>
2024-04-22 19:11:35 -0700

    new

---
+++ tests/__init__.py
@@ -0,0 +1,8 @@
+"""Test suites.
+
+For continuous testing of code (and tests) in a module, install `entr`
+and run this (example for `tests`):
+
+$ ack --type=python biblelib tests -f | entr -r poetry run pytest tests
+
+"""
---
+++ tests/word/__init__.py
@@ -0,0 +1,8 @@
+"""Test suites.
+
+For continuous testing of code (and tests) in a module, install `entr`
+and run this (example for `tests`):
+
+$ ack --type=python biblelib/word tests/word -f | entr -r poetry run pytest tests/word
+
+"""
