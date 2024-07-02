030bca759b9813b5b348ff3633bc58215f1d00c3
Sean Boisen <sean@seanboisen.com>
2024-07-02 16:45:42 -0700

    Added has_connection()

--- biblelib/__init__.py
+++ biblelib/__init__.py
@@ -4,3 +4,30 @@ This namespace is intentionally bare: see individual packages
 (`books`, `words`, etc.) for imports.
 
 """
+
+import requests
+
+
+def has_connection() -> bool:
+    """Return True if there is an active network connection."""
+    try:
+        _ = requests.head(url="http://example.com/", timeout=5)
+        return True
+    except requests.ConnectionError:
+        print("--- No internet connection.")
+        return False
+
+
+__all__ = ["has_connection"]
+
+
+# this is a bit slow to load: is it worth it?
+# you can still load it directly with
+# from biblelib.word import ubs
+# try:
+#     _ = requests.head(url="http://example.com/", timeout=5)
+#     from .ubs import fromubs
+
+#     _exportlist.append("fromubs")
+# except requests.ConnectionError:
+#     print("No internet connection, cannot load ubs.fromubs(), other functionality")
